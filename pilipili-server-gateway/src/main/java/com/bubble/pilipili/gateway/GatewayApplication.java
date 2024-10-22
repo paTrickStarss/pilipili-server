2782d4dc297c2177f38b54cacf762ed5aa0d49fb
liweixin <liweixin@hcrc1.wecom.work>
2024-10-22 17:59:40 +0800

    modify bootstrap.yml

--- pilipili-server-gateway/src/main/java/com/bubble/pilipili/gateway/GatewayApplication.java
+++ pilipili-server-gateway/src/main/java/com/bubble/pilipili/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.bubble.pilipili.server.gateway;
+package com.bubble.pilipili.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
